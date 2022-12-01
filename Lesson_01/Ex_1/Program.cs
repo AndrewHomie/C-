d63731cfa97a93462f3591f20a0203b92a9a5e1f
AndrewHomie <andrey.zemliak@gmail.com>
2022-12-01 21:05:24 +0300

    Сделали певрую задачу семинара

---
+++ Lesson_01/Ex_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("start");
+
+int number = 13; // Определили целое число
+int result = number * number; // Вычесление квадрата числа
+Console.WriteLine(result); // Вывод результата
+
+Console.WriteLine("end"); // окончание
